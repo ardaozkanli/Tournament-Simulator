f2284dfc607f3bd746cc7c36514df9d6b9ebb326
ardaozkanli <ardaozkanli@hotmail.com>
2024-06-20 01:44:23 +0300

    Group Stage updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar"
-import Banner from "./Banner";
+
 import GroupStage from "./GroupStage";
 import ThirdPlace from "./ThirdPlace";
 import PlayOff from "./PlayOff";
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Banner />
+    
       <GroupStage />
       <ThirdPlace />
       <PlayOff />
--- src/GroupStage.jsx
+++ src/GroupStage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import flagsData from "./flags/country-flag.json";
 
 function GroupStage() {
-  return (
-    <div>GroupStage</div>
-  )
+  const [countrys, setCountrys] = useState(flagsData);
+
+  const chunkSize = 4;
+  const groups = {};
+
+  for (let i = 0; i < countrys.length; i += chunkSize) {
+    const groupName = `Group ${String.fromCharCode(65 + i / chunkSize)}`; 
+    groups[groupName] = countrys.slice(i, i + chunkSize);
+  }
+
+  console.log(groups); 
+  return <div>Group Stage</div>;
 }
 
-export default GroupStage
+export default GroupStage;
